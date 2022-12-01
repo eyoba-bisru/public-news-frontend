c4344b06d58428a41236732e07cab968fde2afe9
eyoba-bisru <eabebe91@gmail.com>
2022-12-01 20:42:06 +0300

    colors added

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,14 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      primary: "#4C230A",
+      secondar: "#A53F2B",
+      background: "#CCC9A1",
+      text: "#280004",
+      adminNav: "#F0FFCE",
+      slate: "#D9D9D9",
+    },
     extend: {},
   },
   plugins: [],
