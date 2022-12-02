3751b1a1113612f9141a6b2cb66f62cfc6d5e9fb
unknown <dessiebahiru79@gmail.com>
2022-12-02 21:08:46 +0300

    admin sidebar added

---
+++ pages/configuration.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SidebarWithHeader from '../components/Sidenav'
+
+const configuration = () => {
+  return (
+    <SidebarWithHeader>
+        <p>Configuration</p>
+    </SidebarWithHeader>
+  )
+}
+
+export default configuration
---
+++ pages/notices.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import SidebarWithHeader from '../components/Sidenav'
+
+const notices = () => {
+  return <SidebarWithHeader>
+    <p>Notices</p>
+  </SidebarWithHeader>
+  
+}
+
+export default notices
