7e9c5821d68d9046c750fced1257b2cd49e04fc3
Natnael .M <znatnaelm@gmail.com>
2022-12-06 10:22:23 +0300

    editor page updated

---
+++ pages/editor/editpost.tsx
@@ -0,0 +1,120 @@
+import { Button } from "@chakra-ui/react";
+import Head from "next/head";
+import React from "react";
+import Editornav from "../../components/Editornav";
+
+const addpost = () => {
+  return (
+    <div>
+      <Head>
+        <title>Edit Posts</title>
+      </Head>
+      <div className="flex flex-col gap-10 relative">
+        <Editornav />
+        <div className="flex flex-col items-center justify-center gap-5">
+          <form className="flex md:flex-row flex-col gap-4">
+            <div className="flex flex-col gap-4">
+              <div className="flex flex-col gap-2 w-full md:w-[450px]">
+                <label htmlFor="title" className="text-text font-medium">
+                  Title <span className="text-red-500">*</span>
+                </label>
+                <input
+                  className=" bg-white rounded appearance-none border-2 border-white w-full py-2 px-4  leading-tight focus:outline-none focus:bg-white focus:border-primary"
+                  id="title"
+                  type="text"
+                  placeholder="title"
+                />
+              </div>
+              <div className="flex flex-col gap-2 md:w-[450px]">
+                <label htmlFor="description" className="text-text font-medium">
+                  Description <span className="text-red-500">*</span>
+                </label>
+                <textarea
+                  name="description"
+                  id="description"
+                  className="resize-y h-[250px] rounded bg-white appearance-none border-2 border-white w-full py-2 px-4 leading-tight focus:outline-none focus:bg-white focus:border-primary"
+                ></textarea>
+              </div>
+              <div className="flex flex-col gap-2 w-full md:w-[450px]">
+                <label htmlFor="image" className="text-text font-medium">
+                  Image <span className="text-red-500">*</span>
+                </label>
+                <span className="sr-only"></span>
+                <input
+                  type="file"
+                  id="image"
+                  className="ml-6 rounded block w-full text-sm file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-primary file:text-white hover:file:bg-secondary"
+                />
+              </div>
+            </div>
+            <div className="flex flex-col gap-7">
+              <div className="flex flex-col gap-2">
+                <label htmlFor="location" className="text-text font-medium">
+                  News Location <span className="text-red-500">*</span>
+                </label>
+                <select
+                  id="location"
+                  className="rounded bg-white border-2 border-white mb-6 text-sm focus:ring-primary focus:border-primary block w-full p-2.5"
+                >
+                  <option value="Ethiopia">Ethiopia</option>
+                  <option value="World">World</option>
+                </select>
+              </div>
+              <div className="flex flex-col gap-2">
+                <label htmlFor="content" className="text-text font-medium">
+                  Content Category <span className="text-red-500">*</span>
+                </label>
+                <select
+                  id="content"
+                  className="rounded bg-white border-2 border-white mb-6 text-sm focus:ring-primary focus:border-primary block w-full py-2.5"
+                >
+                  <option value="Sport">Sport</option>
+                  <option value="Education">Education</option>
+                  <option value="Health">Health</option>
+                </select>
+              </div>
+              <div className="flex flex-col gap-2">
+                <label htmlFor="language" className="text-text font-medium">
+                  Language Category <span className="text-red-500">*</span>
+                </label>
+                <select
+                  id="language"
+                  className="rounded bg-white border-2 border-white mb-6 text-sm focus:ring-primary focus:border-primary block w-full p-2.5"
+                >
+                  <option value="Amharic">Amharic</option>
+                  <option value="English">English</option>
+                </select>
+              </div>
+              <div className="flex flex-col gap-2 w-full md:w-[450px]">
+                <label htmlFor="video" className="text-text font-medium">
+                  Youtube Video <span className="text-red-500">*</span>
+                </label>
+                <input
+                  className=" bg-white rounded appearance-none border-2 border-white w-full py-2 px-4  leading-tight focus:outline-none focus:bg-white focus:border-primary"
+                  id="video"
+                  type="text"
+                  placeholder="video link"
+                />
+              </div>
+            </div>
+          </form>
+          <Button
+            bg="#4C230A"
+            marginBlock="14"
+            _hover={{ bg: "#A53F2B" }}
+            color="white"
+            width="30%"
+            paddingY="5"
+          >
+            Edit
+          </Button>
+        </div>
+        <div className="absolute bottom-2 right-[80px]">
+          <p className="text-text">@ 2022 END Media Network</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default addpost;
---
+++ pages/editor/show.tsx
@@ -0,0 +1,70 @@
+import { Avatar } from "@chakra-ui/react";
+import Head from "next/head";
+import Image from "next/image";
+import React from "react";
+import Editornav from "../../components/Editornav";
+const show = () => {
+  return (
+    <div>
+      <Head>
+        <title>Show Post</title>
+      </Head>
+      <Editornav />
+      <div className="flex justify-center items-center">
+        <div className="flex justify-center h-screen">
+          <div className="flex flex-col gap-2 items-center h-full">
+            <div className="flex gap-2 justify-center md:ml-[20%] md:mr-[20%] ml-[5%] mr-[5%] h-full">
+              <Avatar
+                name="EBC"
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/54/EBC_logo.svg/1200px-EBC_logo.svg.png"
+                className="object-contain object-center"
+              />
+
+              <div className="flex flex-col gap-1">
+                <p>Ethiopian Broadcasting Corporation</p>
+                <div className="flex gap-4">
+                  <p>11:00pm</p>
+                  <div className="flex gap-1">
+                    <p>2</p>
+                    <p>sep</p>
+                    <p>2022</p>
+                  </div>
+                </div>
+                <div>
+                  <img
+                    src="https://gdb.voanews.com/A0E0EBFD-8F5A-434D-8A0D-6E3C8C3CCE65_w1023_r1_s.jpg"
+                    alt="image"
+                    className="w-full"
+                  />
+                </div>
+                <div className="flex flex-col gap-3">
+                  <p>Ethiopian Flag Day</p>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Quas facilis eius sapiente beatae exercitationem. Iusto
+                    quibusdam assumenda qui nihil sint perspiciatis sequi quam.
+                    Animi omnis voluptatum quo debitis quod laudantium. Lorem
+                    ipsum dolor sit amet consectetur adipisicing elit. Sunt,
+                    quasi eum omnis distinctio molestias accusantium similique
+                    aliquam obcaecati tempora vitae perspiciatis repellendus
+                    debitis eveniet, nulla tempore quisquam doloremque sequi
+                    facere. Lorem ipsum dolor sit, amet consectetur adipisicing
+                    elit. Ex ratione natus dignissimos optio rem laborum enim
+                    suscipit omnis nobis, perspiciatis, consequuntur quidem
+                    excepturi possimus impedit aperiam dolore quos. Autem,
+                    voluptatibus! Lorem ipsum dolor sit amet consectetur
+                    adipisicing elit. Optio voluptatem hic eum eaque aperiam
+                    consequuntur, iste provident nisi tenetur veniam similique
+                    ducimus quas dignissimos maiores iusto sed est id quibusdam.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default show;
