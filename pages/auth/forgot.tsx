4e1d25bb0e352c1c9c23c8d889db7bd09bc9271c
Natnael-M3 <znatnaelm@gmail.com>
2022-12-09 10:23:37 +0300

    forgot password updated

---
+++ pages/auth/forgot.tsx
@@ -0,0 +1,44 @@
+import { Button, Input, InputGroup, InputLeftElement } from '@chakra-ui/react'
+import Head from 'next/head'
+import UserNavbar from '../../components/UserNavbar'
+import PasswordInput from '../../components/PasswordInputSignup'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+const forgot = () => {
+  return (
+    <div className='w-screen flex flex-col justify-between items-center'>
+      <Head>
+        <title>Forgot Password</title>
+      </Head>
+      <UserNavbar />
+      <div className='w-[314.705px] py-[48.6px] mt-[6rem]'>
+        <div className='w-full bg-white rounded-md h-[200px]'>
+          <div className='px-6 py-1 flex flex-col gap-6'>
+            <p className='font-light text-2xl text-center mt-6'>
+              Forgot Password
+            </p>
+            <form className='flex flex-col justify-center items-center gap-4'>
+              <Input
+                type='email'
+                borderColor='#4C230A'
+                placeholder='Email'
+                _focus={{ border: 'none' }}
+              />
+              <Button
+                bg='#4C230A'
+                _hover={{ bg: '#A53F2B' }}
+                color='white'
+                width='full'
+                paddingY='5'
+              >
+                Reset
+              </Button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default forgot
