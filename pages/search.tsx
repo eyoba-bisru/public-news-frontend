e983dd48711751c88d9eb6a485f299f8aabbc768
Eyob <eabebe91@gmail.com>
2023-01-31 21:20:17 +0300

    search functionality added

--- pages/search.tsx
+++ pages/search.tsx
@@ -6,20 +6,54 @@ import React, { useEffect, useState } from "react";
 import axiosInstance from "../lib/axiosInstance";
 
 import Head from "next/head";
+import instance from "../lib/axiosInstance";
+import Card from "../components/Card";
 type Data = {
   id: string;
   name: string;
   type: string;
 }[];
-const search = ({
-  content,
-  site: sitemap,
-  lang: languages,
-}: {
+
+type Posts = {
+  id: string;
+  title: string;
+  description: string;
+  imageUrl: string;
+  location: {
+    name: string;
+  };
+}[];
+
+type Props = {
   content: Data;
   site: Data;
   lang: Data;
-}) => {
+};
+
+const search = ({ content, site: sitemap, lang: languages }: Props) => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [results, setResults] = useState<Posts>([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      try {
+        const { data } = await instance.post("/post/search", { q: searchTerm });
+        setResults(data);
+      } catch (error) {
+        console.log(error);
+      }
+      setIsLoading(false);
+    };
+
+    if (searchTerm) {
+      fetchData();
+    } else {
+      setResults([]);
+    }
+  }, [searchTerm]);
+
   return (
     <div className="flex flex-col gap-4 relative h-screen">
       <Head>
@@ -30,33 +64,43 @@ const search = ({
       </div>
       <div className="flex flex-col gap-4 items-center">
         <div className="flex items-center gap-1 w-full justify-center mt-[66px] relative">
-          <form className="flex items-center w-full gap-1 justify-center">
-            <select
-              id="content"
-              className=" bg-white md:w-[15%] sm:w-[20%] w-[30%] rounded-tl rounded-bl-none rounded-tr-none rounded-rl-none border-2 border-white py-[7px] px-4 leading-tight focus:outline-none focus:bg-white focus:border-primary"
-            >
-              <option value="all">All</option>
-              {content.map((d) => (
-                <option key={d.id} value={d.name.toLowerCase()}>
-                  {d.name}
-                </option>
-              ))}
-            </select>
+          <form className="flex items-center gap-2 justify-center">
             <input
-              className="bg-white w-[60%] rounded-tl-none rounded-bl-none rounded-tr rounded-br-none  border-2 border-white py-[8px] px-4 leading-tight focus:outline-none focus:bg-white focus:border-primary"
+              className="bg-white rounded-md border-2 border-white py-[8px] px-4 leading-tight focus:outline-none focus:bg-white focus:border-primary"
               id="title"
               type="text"
               placeholder="Search"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
             />
             <button
               type="submit"
-              className="absolute text-text md:ml-[70%] ml-[75%]"
+              className="text-white bg-primary rounded-md px-2 py-1"
             >
-              <BsSearch />
+              Search
             </button>
           </form>
         </div>
-        <div className="h-auto py-12 flex justify-center items-center md:w-[75.15%] sm:w-[80.15%] w-[90.15%] md:h-[187.58px] bg-primary text-white rounded-tl-none rounded-bl rounded-tr-none rounded-br">
+        <div>
+          {isLoading ? (
+            <img src="/Spinner.svg" alt="spinner" />
+          ) : results.length == 0 && searchTerm != "" ? (
+            <div>No result found</div>
+          ) : (
+            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
+              {results.map((result) => (
+                <Link key={result.id} href={`/detail/${result.id}`}>
+                  <Card
+                    image={result.imageUrl}
+                    location={result.location.name}
+                    title={result.title}
+                  />
+                </Link>
+              ))}
+            </div>
+          )}
+        </div>
+        <div className="relative h-auto py-12 flex justify-center items-center w-full bg-primary text-white rounded-tl-none rounded-bl rounded-tr-none rounded-br">
           <div className="w-full max-w-[1200px]">
             <div className="flex justify-center items-center md:flex-row flex-col md:gap-14 gap-10">
               <div className="flex sm:flex-row mt-[2px] sm:mt-[0px] flex-col gap-2 sm:gap-4 justify-center items-center md:block">
@@ -87,13 +131,13 @@ const search = ({
               </div>
             </div>
           </div>
+          <div className="bottom-2 right-[80px] absolute">
+            <p className="text-white text-[11.865px] font-medium">
+              <BiCopyright className="inline" fontSize="14px" />
+              <span>2022 END Media Network</span>
+            </p>
+          </div>
         </div>
-      </div>
-      <div className="absolute bottom-2 right-[80px]">
-        <p className="text-text text-[11.865px] font-medium">
-          <BiCopyright className="inline" fontSize="14px" />
-          <span>2022 END Media Network</span>
-        </p>
       </div>
     </div>
   );
